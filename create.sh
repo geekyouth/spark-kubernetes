11eb87d3f3e7dc7ea9ecc3dc258b75c444e2a153
geekyouth <forsupergeeker@gmail.com>
2022-12-15 14:10:54 +0800

    spark on okteto cloud

--- create.sh
+++ create.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-kubectl create -f ./kubernetes/spark-master-deployment.yaml
-kubectl create -f ./kubernetes/spark-master-service.yaml
+kubectl create -f ./kubernetes/spark-master-deployment.yaml -n hadoop-geekyouth
+kubectl create -f ./kubernetes/spark-master-service.yaml -n hadoop-geekyouth
 
 sleep 10
 
-kubectl create -f ./kubernetes/spark-worker-deployment.yaml
-kubectl apply -f ./kubernetes/minikube-ingress.yaml
+kubectl create -f ./kubernetes/spark-worker-deployment.yaml -n hadoop-geekyouth
+kubectl create -f ./kubernetes/spark-worker-service.yaml -n hadoop-geekyouth
+
+# kubectl apply -f ./kubernetes/minikube-ingress.yaml -n hadoop-geekyouth
+
+kubectl get pods -o wide -n hadoop-geekyouth
